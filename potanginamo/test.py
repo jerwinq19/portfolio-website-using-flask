5a30af49e4ef909052c4825933b648f0eb537ab3
Jerwin Nico <jerwinquijano19@gmail.com>
2024-09-29 01:38:42 +0800

    first github commit yey

--- potanginamo/test.py
+++ potanginamo/test.py
@@ -1,9 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return render_template('index.html')
+def home():
+    return render_template('home.html')
+    
+@app.route('/projects')
+def projects():
+    return render_template('projects.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contacts.html')
+def contacts():
+    return render_template('contacts.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
